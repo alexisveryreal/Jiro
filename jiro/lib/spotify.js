a2b3895afdb69399a52f0e1deb9747110977b5e0
alexisveryreal <alexmaple20@gmail.com>
2021-12-10 23:46:55 -0800

    * have added spotify authetntication     * need to add middleware tomorrow...

---
+++ jiro/lib/spotify.js
@@ -0,0 +1,33 @@
+import SpotifyWebAPi from "spotify-web-api-node";
+
+const scopes = [
+  "user-read-private",
+  "user-read-email",
+  "playlist-read-private",
+  "playlist-read-collaborative",
+  "streaming",
+  "user-library-read",
+  "user-top-read",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-follow-read",
+].join(",");
+
+const params = {
+  scope: scopes,
+};
+
+const queryParamString = new URLSearchParams(params);
+
+const LOGIN_URL = `https://accounts.spotify.com/authorize?${queryParamString.toString()}`;
+
+const spotifyAPI = new SpotifyWebAPi({
+  clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+  clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+});
+
+export default spotifyAPI;
+
+export { LOGIN_URL };
